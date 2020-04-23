e4fe3e455c38c98b76288db4da676a6f39670cd5
kodebae <karmenuopx@outlook.com>
2020-04-22 22:37:34 -0400

    not sure why my code it throwing errors

--- index.js
+++ index.js
@@ -9,14 +9,14 @@
 
 // EXAMPLE SOLUTION CODE:
 function Airplane(name) {
-  this.name = name;
-  this.isFlying = false;
+    this.name = name;
+    this.isFlying = false;
 }
-Airplane.prototype.takeOff = function () {
-  this.isFlying = true;
+Airplane.prototype.takeOff = function() {
+    this.isFlying = true;
 };
-Airplane.prototype.land = function () {
-  this.isFlying = false;
+Airplane.prototype.land = function() {
+    this.isFlying = false;
 };
 
 
@@ -39,33 +39,78 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.stomach = [];
 
+    Person.prototype.eat = function(edible) {
+        if (this.stomach.length < 10) {
+            this.stomach.push(edible);
+        }
+    }
+    Person.prototype.poop = function() {
+        this.stomach = [];
+    }
+    Person.prototype.eat = function(edible) {
+        if (this.stomach.length < 10) {
+            this.stomach.push(edible);
+        }
+    }
+    Person.prototype.poop = function() {
+        this.stomach = [];
+    }
+    Person.prototype.toString = function() {
+        return `${this.name}, ${this.age}`;
+    }
 }
-
 /*
   TASK 2
-    - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
+    - Write a Car constructor that initializes `
+    model ` and `
+    milesPerGallon ` from arguments.
     - All instances built with Car:
-        + should initialize with an `tank` at 0
-        + should initialize with an `odometer` at 0
-    - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
-    - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
-        + Should cause the `odometer` to go up.
-        + Should cause the the `tank` to go down taking `milesPerGallon` into account.
-    - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
-        + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
+        + should initialize with an `
+    tank ` at 0
+        + should initialize with an `
+    odometer ` at 0
+    - Give cars the ability to get fueled with a `.fill(gallons)
+    ` method. Add the gallons to `
+    tank `.
+    - STRETCH: Give cars ability to `.drive(distance)
+    `. The distance driven:
+        + Should cause the `
+    odometer ` to go up.
+        + Should cause the the `
+    tank ` to go down taking `
+    milesPerGallon ` into account.
+    - STRETCH: A car which runs out of `
+    fuel ` while driving can't drive any more distance:
+        + The `
+    drive ` method should return a string "I ran out of fuel at x miles!" x being `
+    odometer `.
 */
 
-function Car() {
+function Car(properties) {
+    this.model = properties.model;
+    this.milesPerGallon = properties.milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+    //get fueled method
+    Car.prototype.gallons = function(); {
 
+    }
 }
 
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
-    - Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
-    - Besides the methods on Person.prototype, babies have the ability to `.play()`:
+    - Besides `
+        name ` and `
+        age `, Baby takes a third argument to initialize `
+        favoriteToy `.
+    - Besides the methods on Person.prototype, babies have the ability to `.play()
+        `:
         + Should return a string "Playing with x", x being the favorite toy.
 */
 function Baby() {
@@ -87,9 +132,9 @@ function Baby() {
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
 if (typeof exports !== 'undefined') {
-  module.exports = module.exports || {}
-  if (Airplane) { module.exports.Airplane = Airplane }
-  if (Person) { module.exports.Person = Person }
-  if (Car) { module.exports.Car = Car }
-  if (Baby) { module.exports.Baby = Baby }
+    module.exports = module.exports || {}
+    if (Airplane) { module.exports.Airplane = Airplane }
+    if (Person) { module.exports.Person = Person }
+    if (Car) { module.exports.Car = Car }
+    if (Baby) { module.exports.Baby = Baby }
 }
